fc31bc2ae69b7e59ed5a0c1d5a8bc6864d5d3444
leonardomuniz <leocontato@outlook.com.br>
2021-11-22 13:59:45 -0300

    fixing navbar responsibility

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -1,16 +1,35 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import { Box, Flex, Spacer } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useBreakpointValue } from "@chakra-ui/react"
 
 
 export default function NavBarComponent() {
+    const variant = useBreakpointValue({ base: "base", sm: 'small', md: "medium", lg: 'large', xl: 'extra large' })
 
+    useEffect(() => { console.log(variant) }, [variant])
     return (
-        <nav className="navbar">
-            <Link to="/" className="logo">Home</Link>
-            <ul className="nav-links">
-                <li><Link to="/search" >Pesquisa avançada</Link></li>
-            </ul>
-        </nav>
+        <>
+            {variant === 'small' || variant === 'base' ? (
+                <Flex className="navbar" fontSize={'120%'} >
+                    <Box style={{marginTop:5}}>
+                        <Link to="/">Home</Link>
+                    </Box>
+                </Flex>
+            ) : (
+                <Flex className="navbar" fontSize={'120%'}>
+                    <Box style={{marginTop:10}}>
+                        <Link to="/">Home</Link>
+                    </Box>
+                    <Spacer />
+                    <Box style={{marginTop:10}} >
+                        <Link to="/search" >Pesquisa avançada</Link>
+                        <Link to="/search" >Gêneros</Link>
+                    </Box>
+                </Flex>
+            )}
+        </>
+
     );
 }
 
